ba10da5eddbda0e49f886c800928628be48c5b79
Rebelus Quo <rebelusquo@tutanota.com>
2019-03-11 21:20:36 +0100

    Unit test most overworld locations
    Adds tests for all remaining locations that don't require dungeon related values.

--- spec/world_spec.js
+++ spec/world_spec.js
@@ -19,16 +19,20 @@ const _with = progress =>
         progress.replace(/ /g, ', ') :
         'nothing'}`;
 
-const update = (tokens, items) =>
+const update = (tokens, items, world) =>
     tokens && tokens.split(' ').forEach(token => {
-        const change = (token, value) => items[token] = value;
-        const values = {
-            mastersword: ['sword', 2],
-            bottle: ['bottle', 1],
-            glove: ['glove', 1],
-            mitt: ['glove', 2]
-        };
-        change(...(values[token] || [token, true]));
+        if (token === 'agahnim')
+            world.castle_tower.completed = true;
+        else {
+            const change = (token, value) => items[token] = value;
+            const values = {
+                mastersword: ['sword', 2],
+                bottle: ['bottle', 1],
+                glove: ['glove', 1],
+                mitt: ['glove', 2]
+            };
+            change(...(values[token] || [token, true]));
+        }
     });
 
 describe('World', () => {
@@ -64,8 +68,40 @@ describe('World', () => {
 
         ['castle_escape', null, 'always'],
 
+        ['darkworld_deathmountain_west', null, false],
+        ['darkworld_deathmountain_west', 'flute', true],
+        ['darkworld_deathmountain_west', 'glove lamp', true],
+
+        ['darkworld_deathmountain_east', null, false],
+        ['darkworld_deathmountain_east', 'mitt hammer mirror flute', true],
+        ['darkworld_deathmountain_east', 'mitt hammer mirror lamp', true],
+        ['darkworld_deathmountain_east', 'mitt hookshot flute', true],
+        ['darkworld_deathmountain_east', 'mitt hookshot lamp', true],
+
+        ['darkworld_northwest', null, false],
+        ['darkworld_northwest', 'moonpearl agahnim hookshot flippers', true],
+        ['darkworld_northwest', 'moonpearl agahnim hookshot glove', true],
+        ['darkworld_northwest', 'moonpearl agahnim hookshot hammer', true],
+        ['darkworld_northwest', 'moonpearl glove hammer', true],
+        ['darkworld_northwest', 'moonpearl mitt', true],
+
+        ['darkworld_northeast', null, false],
+        ['darkworld_northeast', 'agahnim', true],
+        ['darkworld_northeast', 'moonpearl glove hammer', true],
+        ['darkworld_northeast', 'moonpearl mitt flippers', true],
+
+        ['darkworld_south', null, false],
+        ['darkworld_south', 'moonpearl agahnim hammer', true],
+        ['darkworld_south', 'moonpearl agahnim hookshot glove', true],
+        ['darkworld_south', 'moonpearl agahnim hookshot flippers', true],
+        ['darkworld_south', 'moonpearl glove hammer', true],
+        ['darkworld_south', 'moonpearl mitt', true],
+
+        ['darkworld_mire', null, false],
+        ['darkworld_mire', 'flute mitt', true],
+
         (region, progress, state) => it(`can enter ${region} ${is(state)} ${_with(progress)}`, () => {
-            update(progress, items);
+            update(progress, items, world);
             state === 'always' ?
                 expect(world[region].can_enter).to.be.falsy :
                 world[region].can_enter({ items, world }).should.equal(state);
@@ -78,6 +114,13 @@ describe('World', () => {
         ['lightworld_deathmountain_east', null, false],
         ['lightworld_deathmountain_east', 'hammer mirror glove', true],
         ['lightworld_deathmountain_east', 'hookshot glove', true],
+
+        ['darkworld_deathmountain_west', null, false],
+        ['darkworld_deathmountain_west', 'glove', true],
+
+        ['darkworld_deathmountain_east', null, false],
+        ['darkworld_deathmountain_east', 'mitt hammer mirror', true],
+        ['darkworld_deathmountain_east', 'mitt hookshot', true],
 
         (region, progress, state) => it(`can enter dark ${region} ${is(state)} ${_with(progress)}`, () => {
             update(progress, items);
@@ -112,6 +155,18 @@ describe('World', () => {
         ['lightworld_northwest', 'tree', null, 'viewable'],
         ['lightworld_northwest', 'graveyard_w', null, false],
         ['lightworld_northwest', 'graveyard_w', 'boots', true],
+        ['lightworld_northwest', 'graveyard_n', null, false],
+        ['lightworld_northwest', 'graveyard_n', 'mirror moonpearl agahnim hookshot flippers', true],
+        ['lightworld_northwest', 'graveyard_n', 'mirror moonpearl agahnim hookshot glove', true],
+        ['lightworld_northwest', 'graveyard_n', 'mirror moonpearl agahnim hookshot hammer', true],
+        ['lightworld_northwest', 'graveyard_n', 'mirror moonpearl glove hammer', true],
+        ['lightworld_northwest', 'graveyard_n', 'mirror moonpearl mitt', true],
+        ['lightworld_northwest', 'graveyard_e', null, false],
+        ['lightworld_northwest', 'graveyard_e', 'boots mitt', true],
+        ['lightworld_northwest', 'graveyard_e', 'boots mirror moonpearl agahnim hookshot flippers', true],
+        ['lightworld_northwest', 'graveyard_e', 'boots mirror moonpearl agahnim hookshot glove', true],
+        ['lightworld_northwest', 'graveyard_e', 'boots mirror moonpearl agahnim hookshot hammer', true],
+        ['lightworld_northwest', 'graveyard_e', 'boots mirror moonpearl glove hammer', true],
         ['lightworld_northwest', 'kid', null, false],
         ['lightworld_northwest', 'kid', 'bottle', true],
         ['lightworld_northwest', 'well', null, 'always'],
@@ -119,6 +174,11 @@ describe('World', () => {
         ['lightworld_northwest', 'bottle', null, 'always'],
         ['lightworld_northwest', 'chicken', null, 'always'],
         ['lightworld_northwest', 'tavern', null, 'always'],
+        ['lightworld_northwest', 'frog', null, false],
+        ['lightworld_northwest', 'frog', 'moonpearl mitt', true],
+        ['lightworld_northwest', 'bat', null, false],
+        ['lightworld_northwest', 'bat', 'powder hammer', true],
+        ['lightworld_northwest', 'bat', 'powder moonpearl mirror mitt', true],
 
         ['lightworld_northeast', 'zora', null, false],
         ['lightworld_northeast', 'zora', 'flippers', true],
@@ -137,12 +197,36 @@ describe('World', () => {
         ['lightworld_south', 'library', 'boots', true],
         ['lightworld_south', 'grove_n', null, false],
         ['lightworld_south', 'grove_n', 'shovel', true],
+        ['lightworld_south', 'grove_s', null, false],
+        ['lightworld_south', 'grove_s', 'mirror moonpearl agahnim hammer', true],
+        ['lightworld_south', 'grove_s', 'mirror moonpearl agahnim hookshot glove', true],
+        ['lightworld_south', 'grove_s', 'mirror moonpearl agahnim hookshot flippers', true],
+        ['lightworld_south', 'grove_s', 'mirror moonpearl glove hammer', true],
+        ['lightworld_south', 'grove_s', 'mirror moonpearl mitt', true],
         ['lightworld_south', 'link_house', null, 'always'],
         ['lightworld_south', 'desert_w', null, 'viewable'],
+        ['lightworld_south', 'desert_w', 'book', true],
+        ['lightworld_south', 'desert_w', 'flute mitt mirror', true],
+        ['lightworld_south', 'desert_ne', null, false],
+        ['lightworld_south', 'desert_ne', 'flute mitt mirror', true],
         ['lightworld_south', 'aginah', null, 'always'],
+        ['lightworld_south', 'bombos', null, false],
+        ['lightworld_south', 'bombos', 'book mirror moonpearl agahnim hammer', 'viewable'],
+        ['lightworld_south', 'bombos', 'book mirror moonpearl agahnim hookshot glove', 'viewable'],
+        ['lightworld_south', 'bombos', 'book mirror moonpearl agahnim hookshot flippers', 'viewable'],
+        ['lightworld_south', 'bombos', 'book mirror moonpearl glove hammer', 'viewable'],
+        ['lightworld_south', 'bombos', 'book mirror moonpearl mitt', 'viewable'],
+        ['lightworld_south', 'bombos', 'book mirror moonpearl agahnim hammer mastersword', true],
+        ['lightworld_south', 'bombos', 'book mirror moonpearl agahnim hookshot glove mastersword', true],
+        ['lightworld_south', 'bombos', 'book mirror moonpearl agahnim hookshot flippers mastersword', true],
+        ['lightworld_south', 'bombos', 'book mirror moonpearl glove hammer mastersword', true],
+        ['lightworld_south', 'bombos', 'book mirror moonpearl mitt mastersword', true],
         ['lightworld_south', 'dam', null, 'always'],
         ['lightworld_south', 'lake_sw', null, 'always'],
         ['lightworld_south', 'island_lake', null, 'viewable'],
+        ['lightworld_south', 'island_lake', 'flippers moonpearl mirror agahnim', true],
+        ['lightworld_south', 'island_lake', 'flippers moonpearl mirror glove hammer', true],
+        ['lightworld_south', 'island_lake', 'flippers moonpearl mirror mitt', true],
         ['lightworld_south', 'hobo', null, false],
         ['lightworld_south', 'hobo', 'flippers', true],
         ['lightworld_south', 'ice_cave', null, 'always'],
@@ -156,8 +240,42 @@ describe('World', () => {
         ['castle_escape', 'castle', null, 'always'],
         ['castle_escape', 'secret', null, 'always'],
 
+        ['darkworld_deathmountain_west', 'spike', null, false],
+        ['darkworld_deathmountain_west', 'spike', 'moonpearl hammer glove cape', true],
+        ['darkworld_deathmountain_west', 'spike', 'moonpearl hammer glove byrna', true],
+
+        ['darkworld_deathmountain_east', 'rock_hook', null, false],
+        ['darkworld_deathmountain_east', 'rock_hook', 'moonpearl hookshot', true],
+        ['darkworld_deathmountain_east', 'rock_boots', null, false],
+        ['darkworld_deathmountain_east', 'rock_boots', 'moonpearl hookshot', true],
+        ['darkworld_deathmountain_east', 'rock_boots', 'moonpearl boots', true],
+        ['darkworld_deathmountain_east', 'bunny', null, false],
+        ['darkworld_deathmountain_east', 'bunny', 'moonpearl', true],
+
+        ['darkworld_northwest', 'bumper', null, 'viewable'],
+        ['darkworld_northwest', 'bumper', 'glove cape', true],
+        ['darkworld_northwest', 'chest_game', null, 'always'],
+        ['darkworld_northwest', 'c_house', null, 'always'],
+        ['darkworld_northwest', 'bomb_hut', null, 'always'],
+        ['darkworld_northwest', 'purple', null, false],
+        ['darkworld_northwest', 'purple', 'mitt', true],
+        ['darkworld_northwest', 'pegs', null, false],
+        ['darkworld_northwest', 'pegs', 'mitt hammer', true],
+
+        ['darkworld_northeast', 'catfish', null, false],
+        ['darkworld_northeast', 'catfish', 'moonpearl glove', true],
+        ['darkworld_northeast', 'pyramid', null, 'always'],
+        ['darkworld_northeast', 'fairy_dw', null, false],
+
+        ['darkworld_south', 'dig_game', null, 'always'],
+        ['darkworld_south', 'stumpy', null, 'always'],
+        ['darkworld_south', 'swamp_ne', null, 'always'],
+
+        ['darkworld_mire', 'mire_w', null, false],
+        ['darkworld_mire', 'mire_w', 'moonpearl', true],
+
         (region, name, progress, state) => it(`can access ${region} - ${name} ${is(state)} ${_with(progress)}`, () => {
-            update(progress, items);
+            update(progress, items, world);
             state === 'always' ?
                 expect(world[region].locations[name].can_access).to.be.falsy :
                 world[region].locations[name].can_access({ items, world, mode }).should.equal(state);
